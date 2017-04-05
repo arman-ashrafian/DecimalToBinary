d73ce131fddb5e138d3c56e2823e12c7aa5f1d3b
Arman Ashrafian <ashy63@gmail.com>
2017-04-05 12:42:33 -0700

    initial commit

---
+++ decimalToBinary.py
@@ -0,0 +1,31 @@
+# show powers of 2 that add up to a decimal number
+import math
+
+def main():
+    binary_nums = []
+    num = float(input("Enter Number: "))
+    power = getStartingPower(num)
+
+    numCopy = num   #starting point
+    while(numCopy > 0):
+        if(numCopy - math.pow(2, power) >= 0):
+            numCopy -= math.pow(2,power)
+            binary_nums.append("2^%d" % power)
+        power -= 1
+
+    outputList(binary_nums)
+
+
+def getStartingPower(num):
+    count = 0
+    while(num > 1):
+        num = num * .5
+        count += 1
+    return count
+
+def outputList(arr):
+    for num in arr:
+        print(num)
+
+if __name__ == '__main__':
+    main()
